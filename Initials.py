91aa698fd012396cba4f99b8369a472ca0d56271
Amal Hirole <142929434+uchihakira43@users.noreply.github.com>
2023-10-10 18:43:18 -0400

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,9 @@
+#Amal Hirole
+#Fun fact: I like dragon boating
+print(' AAA ' + '   H   H')
+print('A   A' + '   H   H')
+print('A   A' + '   H   H')
+print('A A A' + '   H H H')
+print('A   A' + '   H   H')
+print('A   A' + '   H   H')
+print('A   A' + '   H   H')
